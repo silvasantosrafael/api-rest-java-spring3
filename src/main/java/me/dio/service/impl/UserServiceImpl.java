11e3fa98ba94dcfa5e3cc0be21da31ee832ad75a
Rafael <rafael.silva318819@gmail.com>
2023-10-19 23:24:21 -0300

    first attempt at publishing on railway

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository repository;
+
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return repository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User user) {
+        if (repository.existsByAccountNumber(user.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This account number already exists.");
+        }
+        return repository.save(user);
+    }
+}
